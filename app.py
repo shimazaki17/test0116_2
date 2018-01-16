d4054f68a8e655574c93cbe56b6609b26c7f7da7
atsushishimazaki <sendatsushimessages@gmail.com>
2018-01-16 15:16:44 +0900

    4

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 
 
 def wsgi_app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    response_body = 'Hello World'
-    yield response_body.encode()
+
+    from flask import render_template
+    return render_template('index.html')
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
