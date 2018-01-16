67790522de050ea626c4f519484cbd9fcd2a6f1b
atsushishimazaki <sendatsushimessages@gmail.com>
2018-01-16 20:30:19 +0900

    5

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ def hello_world():
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
 
-    response_body = 'Hello World'
-    yield response_body.encode()
+    return [b"Hello World"]
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 5555, hello_world)
+    print("Serving on port 8000...")
     httpd.serve_forever()
