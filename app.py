a490f6562d1669223061be449586e242173481b2
atsushishimazaki <sendatsushimessages@gmail.com>
2018-01-16 19:46:39 +0900

    1

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
 def hello_world():
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
+
     response_body = 'Hello World'
     yield response_body.encode()
 
-if __name__ == '__main__':
-    app.run()
+from wsgiref.simple_server import make_server
+httpd = make_server('localhost', 5555, hello_world)
+httpd.serve_forever()
