5ba78dd4fd95dbf342a2e2eec65cdedb9abf0252
atsushishimazaki <sendatsushimessages@gmail.com>
2018-01-16 15:25:33 +0900

    2

--- app.py
+++ app.py
@@ -6,7 +6,4 @@ def wsgi_app(environ, start_response):
     return render_template('index.html')
 
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
-
-    httpd = make_server('localhost', 5555, wsgi_app)
-    httpd.serve_forever()
+    wsgi_app.run()
