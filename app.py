77c702f14fc9f32d58a2ec87cc4d8fe81dec09d9
atsushishimazaki <sendatsushimessages@gmail.com>
2018-01-16 17:14:13 +0900

    1

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
-from flask import Flask, render_template, request, session
-from flask_pymongo import PyMongo
-from pymongo import MongoClient
-import urllib.request
-import json
-from bs4 import BeautifulSoup
-from bson import ObjectId
-from bson.son import SON
-
-from selenium import webdriver
-import time
-
+from flask import Flask
 app = Flask(__name__)
-mongo = PyMongo(app)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run()
+def hello_world():
+    return 'Hello, World!'
