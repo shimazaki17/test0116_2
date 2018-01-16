6cf78e5594c1445fd194deedc007c96ca0d45588
atsushishimazaki <sendatsushimessages@gmail.com>
2018-01-16 16:35:48 +0900

    1

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
+from flask import Flask, render_template, request, session
+from flask_pymongo import PyMongo
+from pymongo import MongoClient
+import urllib.request
+import json
+from bs4 import BeautifulSoup
+from bson import ObjectId
+from bson.son import SON
 
+from selenium import webdriver
+import time
 
-def wsgi_app(environ, start_response):
+app = Flask(__name__)
+mongo = PyMongo(app)
 
-    from flask import render_template
+@app.route('/')
+def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    wsgi_app.run()
+if __name__ == "__main__":
+    app.run()
